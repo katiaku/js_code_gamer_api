6d69fd9cd6cdea57e5afeb3ca6d2d054b6e029dd
Katia <kushnir.ekaterina@gmail.com>
2023-12-30 18:34:59 +0100

    Actualizacion de getThemes para la funcionalidad de la pag. intro-niveles

--- src/controllers/themes.controller.js
+++ src/controllers/themes.controller.js
@@ -6,7 +6,7 @@ const getThemes = async (req, res) => {
         let params = [req.query.iduser, req.query.id_level];
         console.log(params);
         let sql;
-        sql = `SELECT l.title AS 'title_level', t.id_level, t.title AS 'theme_title', t.content, t.code FROM themes AS t
+        sql = `SELECT l.title AS 'title_level', l.intro, t.id_level, t.title AS 'theme_title', t.content, t.code FROM themes AS t
             INNER JOIN levels AS l ON (l.idlevels = t.id_level)
             INNER JOIN user_theme AS ut ON (ut.idtheme = t.idthemes) 
             WHERE ut.iduser = ? AND t.id_level = ?`;
