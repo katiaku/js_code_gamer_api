c2fa4b61d85724cddb240c9e657e58eb8acdd2b6
indiana <indimar.w1@gmail.com>
2024-01-08 09:43:21 +0100

    Merge branch 'main' of https://github.com/katiaku/js_code_gamer_api

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -67,44 +67,35 @@ const userRetos = async (req, res) => {
     }
 };
 
-// const userLogin = async (req, res) => {
-//     try {
-//         let sql = "SELECT * FROM user WHERE email = ? AND password = ?";
-//         let [result] = await pool.query(sql, [req.body.email, req.body.password]);
-
-//         if (result.length > 0) {
-//             const userWithoutPassword = { ...result[0], password: undefined };
-//             res.status(200).json(userWithoutPassword);
-//         } else {
-//             res.status(401).send('Credenciales incorrectas');
-//         }
-//     } catch (error) {
-//         console.error("Error al realizar la consulta de inicio de sesión:", error);
-//         res.status(500).send('Error interno del servidor');
-//     }
-// };
-
 const userLogin = async (req, res) => {
     try {
         let sql = `
-        SELECT 
-        user.*,
-        user_theme.iduser AS user_theme_iduser,
-        themes.id_level AS themes_id_level,
-        user_challenges.iduser AS user_challenges_iduser,
-        challenges.id_level AS challenges_id_level,
-        levels.idlevels AS levels_idlevels,
-        user_level.iduser AS user_level_iduser,
-        user_level.idlevel AS user_level_idlevel
-    FROM user
-    LEFT JOIN user_theme ON user.iduser = user_theme.iduser
-    LEFT JOIN themes ON user_theme.idtheme = themes.idthemes
-    LEFT JOIN user_challenges ON user.iduser = user_challenges.iduser
-    LEFT JOIN challenges ON user_challenges.idchallenge = challenges.idchallenges
-    LEFT JOIN user_level ON user.iduser = user_level.iduser
-    LEFT JOIN levels ON user_level.idlevel = levels.idlevels
-WHERE 
-    user.email = ? AND user.password = ?`;
+        SELECT
+	user.*,
+    user_theme.iduser AS user_theme_iduser,
+    themes.id_level AS theme_id_level,
+    user_challenges.iduser AS user_challenges_iduser,
+    challenges.id_level AS challenge_id_level,
+    levels.idlevels,
+    user_level.iduser AS user_level_iduser,
+    user_level.idlevel AS user_level_idlevel
+FROM
+    user
+JOIN
+    user_theme ON user.iduser = user_theme.iduser
+JOIN
+    themes ON user_theme.idtheme = themes.idthemes
+JOIN
+    user_challenges ON user.iduser = user_challenges.iduser
+JOIN
+    challenges ON user_challenges.idchallenge = challenges.idchallenges
+JOIN
+    levels ON themes.id_level = levels.idlevels
+JOIN
+    user_level ON user.iduser = user_level.iduser
+WHERE
+    user.email = ?
+    AND user.password = ?`;
         let [result] = await pool.query(sql, [req.body.email, req.body.password]);
 
         if (result.length > 0) {
@@ -123,100 +114,189 @@ WHERE
 const avancePorcentaje = async (req, res) => {
     try{
         const iduserTheme = req.body.iduserTheme;
-        const id_levelTheme = req.body.id_levelTheme;
+        // const id_levelTheme = req.body.id_levelTheme;
         const iduserChallenges = req.body.iduserChallenges;
-        const id_levelChallenges = req.body.id_levelChallenges;
-        const idlevelsLevels = req.body.idlevelsLevels;
+        // const id_levelChallenges = req.body.id_levelChallenges;
+        // const idlevelsLevels = req.body.idlevelsLevels;
         const iduserUserLevel = req.body.iduserUserLevel;
-        const idlevelUserLevel = req.body.idlevelUserLevel;
+        // const idlevelUserLevel = req.body.idlevelUserLevel;
         
         console.log(iduserTheme)
-        console.log(id_levelTheme)
+        // console.log(id_levelTheme)
         console.log(iduserChallenges)
-        console.log(id_levelChallenges)
-        console.log(idlevelsLevels)
+        // console.log(id_levelChallenges)
+        // console.log(idlevelsLevels)
         console.log(iduserUserLevel)
-        console.log(idlevelUserLevel)
-        const sql = `
-        UPDATE user_level
+        // console.log(idlevelUserLevel)
+
+        const updateSql = `
+        UPDATE user_level AS ul
         SET percentage = (
             ((
                 SELECT COUNT(*)
-                FROM user_theme JOIN themes ON (themes.idthemes = user_theme.idtheme)
-                WHERE user_theme.iduser = ? AND user_theme.completed = 1 AND themes.id_level = ?
+                FROM user_theme AS ut
+                JOIN themes AS t ON t.idthemes = ut.idtheme
+                WHERE ut.iduser = ? AND ut.completed = 1 AND t.id_level = ul.idlevel
                 )+(
                  SELECT COUNT(*)
-                FROM user_challenges JOIN challenges ON (challenges.idchallenges = user_challenges.idchallenge)
-                WHERE user_challenges.iduser = ? AND user_challenges.completed = 1 AND challenges.id_level = ?
+                FROM user_challenges AS uc
+                JOIN challenges AS c ON c.idchallenges = uc.idchallenge
+                WHERE uc.iduser = ? AND uc.completed = 1 AND c.id_level = ul.idlevel
                 ))
             /
             (
-                SELECT levels.number_challenge + levels.number_theme FROM levels WHERE levels.idlevels = ?
+                SELECT l.number_challenge + l.number_theme 
+                FROM levels AS l 
+                WHERE l.idlevels = ul.idlevel
             )
         ) * 100
-        WHERE user_level.iduser = ? AND  user_level.idlevel = ?;
+        WHERE ul.iduser = ? AND  ul.idlevel = (SELECT idlevels FROM levels WHERE idlevels = ul.idlevel);
+
         `;
 
-       let [result] = await pool.query(sql, [iduserTheme, id_levelTheme, iduserChallenges, id_levelChallenges, idlevelsLevels, iduserUserLevel, idlevelUserLevel]);
+        const selectSql = `
+            SELECT percentage
+            FROM user_level
+            WHERE user_level.iduser = ? AND user_level.idlevel = user_level.idlevel;
+        `;
+
+       await pool.query(updateSql, [iduserTheme, iduserChallenges, iduserUserLevel]);
+       const [result] = await pool.query(selectSql, [iduserUserLevel]);
+       if (result.length > 0) {
+        const nuevoPorcentaje = result[0].percentage;
+        console.log(nuevoPorcentaje);
        
         console.log(iduserTheme)
-        console.log(id_levelTheme)
+        // console.log(id_levelTheme)
         console.log(iduserChallenges)
-        console.log(id_levelChallenges)
-        console.log(idlevelsLevels)
+        // console.log(id_levelChallenges)
+        // console.log(idlevelsLevels)
         console.log(iduserUserLevel)
-        console.log(idlevelUserLevel)
+        // console.log(idlevelUserLevel)
 
         console.log(result)
-        res.status(200).json({ success: true, message: 'Porcentaje actualizado con éxito', result });
+        res.status(200).json({ success: true, message: 'Porcentaje actualizado con éxito', porcentaje: nuevoPorcentaje, result });
+    } else {
+        // La actualización no fue exitosa
+        res.status(500).json({ success: false, error: 'La actualización del porcentaje no fue exitosa', result });
+    } 
     } catch (error) {
         console.error('Error al ejecutar la consulta:', error);
         res.status(500).json({ success: false, error: 'Error interno del servidor' });
     }
 };
 
+const obtenerDatosNiveles = async (req, res) => {
+    try {
+      const iduser = req.params.iduser;
+      const sql = `SELECT *, title
+      FROM user_level 
+      JOIN levels ON user_level.idlevel = levels.idlevels
+      WHERE iduser = ?`;
+      const [result] = await pool.query(sql, [iduser]);
+  
+      res.status(200).json(result);
+    } catch (error) {
+      console.error('Error al obtener datos de niveles:', error);
+      res.status(500).json({ error: 'Error interno del servidor' });
+    }
+  };
 
+
+//   const updateUser = async (request, response) => {
+//     try {
+//       const { iduser, name_surname, email, username, photo, password} = request.body;
+  
+//            if (!name_surname || !email || !username || !photo || !password) {
+//         return response.status(400).send({ error: true, codigo: 400, message: "Datos incompletos para actualizar el usuario" });
+//       }
+  
+//       // Realiza la actualización en la base de datos
+//       const sql = 'UPDATE user SET name_surname = ?, email = ?, username = ?, photo = ?, password = ? WHERE iduser = ?;';
+//       const [result] = await pool.query(sql, [name_surname, email, username, photo, password, iduser]);
+//       console.log(result)
+  
+//       // Verifica si la actualización fue exitosa
+//       if (result.affectedRows === 1) {
+//         response.send({ error: false, codigo: 200, message: "Usuario actualizado correctamente" });
+//       } else {
+//         response.status(500).send({ error: true, codigo: 500, message: "No se pudo actualizar el Usuario" });
+//       }
+//     } catch (error) {
+//       console.error("Error:", error);
+//       response.status(500).send({ error: true, codigo: 500, message: "Error interno del servidor" });
+//     }
+//   };
+
+
+const updateUser = async (request, response) => {
+    try {
+      const { iduser, name_surname, email, username, photo, password } = request.body;
+  
+      if (!iduser) {
+        return response.status(400).send({ error: true, codigo: 400, message: "ID de usuario no proporcionado" });
+      }
+  
+      let updateFields = [];
+      let updateValues = [];
+  
+      if (name_surname) {
+        updateFields.push('name_surname = ?');
+        updateValues.push(name_surname);
+      }
+  
+      if (email) {
+        updateFields.push('email = ?');
+        updateValues.push(email);
+      }
+  
+      if (username) {
+        updateFields.push('username = ?');
+        updateValues.push(username);
+      }
+  
+      if (photo) {
+        updateFields.push('photo = ?');
+        updateValues.push(photo);
+      }
+  
+      if (password) {
+        updateFields.push('password = ?');
+        updateValues.push(password);
+      }
+  
+      if (updateFields.length === 0) {
+        return response.status(400).send({ error: true, codigo: 400, message: "No hay datos para actualizar" });
+      }
+  
+      // Construye la consulta SQL dinámicamente
+      const sql = `UPDATE user SET ${updateFields.join(', ')} WHERE iduser = ?;`;
+      const queryValues = [...updateValues, iduser];
+  
+      console.log("SQL:", sql);
+      console.log("Query Values:", queryValues);
+
+      // Realiza la actualización en la base de datos
+      const [result] = await pool.query(sql, queryValues);
+      console.log("Query Result:", result);
+  
+      // Verifica si la actualización fue exitosa
+      if (result.affectedRows === 1) {
+        response.send({ error: false, codigo: 200, message: "Usuario actualizado correctamente" });
+      } else {
+        response.status(500).send({ error: true, codigo: 500, message: "No se pudo actualizar el Usuario" });
+      }
+    } catch (error) {
+      console.error("Error:", error);
+      response.status(500).send({ error: true, codigo: 500, message: "Error interno del servidor" });
+    }
+  };
 
 module.exports = {
     userRegister,
     userLogin,
     userRetos,
+    updateUser,
     avancePorcentaje,
+    obtenerDatosNiveles,
 };
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// SELECT 
-// 	user.*,
-// 	user_theme.iduser AS iduserTheme,
-// 	themes.id_level AS id_levelTheme,
-// 	user_challenges.iduser AS iduserChallenges,
-// 	challenges.id_level AS id_levelChallenges,
-//     levels.idlevels AS idlevelsLevels,
-//     user_level.iduser AS iduserUserLevel,
-//     user_level.idlevel AS idlevelUserLevel
-// FROM 
-// 	user
-//     LEFT JOIN user_theme ON user.iduser = user_theme.iduser
-//     LEFT JOIN themes ON levels.idlevels = themes.id_level
-//     LEFT JOIN user_challenges ON user.iduser = user_challenges.iduser
-//     LEFT JOIN challenges ON levels.idlevels = challenges.id_level
-//     LEFT JOIN levels ON levels.idlevels = levels.idlevels
-//     LEFT JOIN user_level ON user.iduser = user_level.iduser
-//     LEFT JOIN user_level ON levels.idlevels = user_level.idlevel
-// WHERE 
-// user.email = 'pepe@mail.com' AND user.password = 'pepe1234';
--- src/routers/router.js
+++ src/routers/router.js
@@ -11,11 +11,13 @@ router.post('/register', controladorUser.userRegister);
 router.post('/login', controladorUser.userLogin);
 
 router.get('/retos', controladorUser.userRetos);
-//http://localhost:3000/login
-// router.post('/actualizarPorcentaje/:iduserTheme/:id_levelTheme/:iduserChallenges/:id_levelChallenges/:idlevelsLevels/:iduserUserLevel/:idlevelUserLevel', controladorUser.avancePorcentaje);
+
+router.put('/usuario', controladorUser.updateUser);
+
 router.post('/actualizarPorcentaje', controladorUser.avancePorcentaje);
 
-//http://localhost:3000/actualizarPorcentaje/24/1/24/1/1/24/1
+router.get('/obtenerDatosNiveles/:iduser', controladorUser.obtenerDatosNiveles)
+
 module.exports = router;
 
 
