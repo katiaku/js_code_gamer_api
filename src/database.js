c1af729c135effefe919cce1c547ad2d9b744c7d
Katia <kushnir.ekaterina@gmail.com>
2024-01-10 13:31:32 +0100

    Cambios para Vercel

--- src/database.js
+++ src/database.js
@@ -1,15 +1,16 @@
 const mysql = require('mysql2');
 
 const pool = mysql.createPool({
-    host: 'db-js-code-gamer.c9dnvksxuzcm.eu-north-1.rds.amazonaws.com',
-    user: 'admin',
-    password: 'KatIndMar',
-    database: 'db-js-code-gamer',
-    waitForConnections: true,
-    connectionLimit: 10,
-    maxIdle: 10,
-    idleTimeout: 60000,
-    queueLimit: 0
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT
+    // waitForConnections: true,
+    // connectionLimit: 10,
+    // maxIdle: 10,
+    // idleTimeout: 60000,
+    // queueLimit: 0
 }).promise();
 
 console.log('Conexión con la BBDD creada');
