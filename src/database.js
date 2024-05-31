f32dd5edb080cbb720408caa09ffa0c2fd508a01
Ekaterina Kushnir <kushnir.ekaterina@gmail.com>
2024-05-31 16:51:50 +0200

    Cambios de acceso a la base de datos

--- src/database.js
+++ src/database.js
@@ -1,16 +1,16 @@
 const mysql = require('mysql2');
 
 const pool = mysql.createPool({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    port: process.env.DB_PORT
-    // waitForConnections: true,
-    // connectionLimit: 10,
-    // maxIdle: 10,
-    // idleTimeout: 60000,
-    // queueLimit: 0
+    // host: 'db-js-code-gamer.c9dnvksxuzcm.eu-north-1.rds.amazonaws.com',
+    host: 'localhost',
+    user: 'admin',
+    password: 'KatIndMar',
+    database: 'db-js-code-gamer',
+    waitForConnections: true,
+    connectionLimit: 10,
+    maxIdle: 10,
+    idleTimeout: 60000,
+    queueLimit: 0
 }).promise();
 
 console.log('Conexión con la BBDD creada');
